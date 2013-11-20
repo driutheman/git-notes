eae7dd6b03340292a305633094f594c95ddabec0
Andrius Každailevičius <muskatas@yahoo.ie>
2013-11-20 12:31:23 -0500

    Update shh.sh

--- shh.sh
+++ shh.sh
@@ -1,25 +1,7 @@
 
 
-
 # ======================
 # Download file with ssh
 # ======================
 
-  # Download file to you machine
   scp andrius@zeel.com:/var/log/kohana/www/2013/06/06.php /home/driu/20130606.log
-
-
-
-# ==================
-# Change permissions
-# ==================
-
-  # only for files
-  sudo find {folder} -type f -print0 | sudo xargs -0 chmod 664
-
-  # only for directories
-  sudo find {folder} -type d -print0 | sudo xargs -0 chmod 775
-  
-  # all files and directories
-  sudo chmod -R 775 *
-  
